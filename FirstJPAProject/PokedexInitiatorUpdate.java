fb009ab36d45b28a040bfd79f359a31871f2ec30
MonalishaPW <monalishadandapat271@gmail.com>
2026-01-02 08:21:58 +0530

    Add files via upload

---
+++ FirstJPAProject/PokedexInitiatorUpdate.java
@@ -0,0 +1,28 @@
+package io.Mona;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class PokedexInitiatorUpdate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EntityManagerFactory entityManagerFactory =Persistence.createEntityManagerFactory("Monalisha");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		 EntityTransaction entityTransaction = entityManager.getTransaction();
+	        
+		Pokemon p = entityManager.find(Pokemon.class,"pikachu");
+		if(p !=null) {
+			p.setPower(200);
+			 entityTransaction.begin();
+			 entityManager.persist(p);
+			 entityTransaction.commit();
+		        
+		}else {
+			System.out.println("entry does not exist");
+		}
+	}
+
+}
